77debfebfa2d60dbad3a428f5bb75efc784dec0c
Trufaldi <97968778+Trufaldi@users.noreply.github.com>
2022-04-07 14:21:48 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,81 +1,95 @@
+// функция валидации полей (форма, массивПолей)
 const validateFields = (form, fieldsArray) => {
 
+    // массивПолей.дляКаждого(поле  => {
     fieldsArray.forEach(field => {
+        // поле.удалитьКласс("input-error");
         field.removeClass("input-error");
+        // если (поле.значение().убратьПробелы() === "")
         if (field.val().trim() === "") {
+            // поле.добавитьКласс("input-error");
             field.addClass("input-error");
         }
     });
-
+    // поляСОшибками = форма.найти(".input-error");
     const errorFields = form.find(".input-error");
+    // вернуть поляСОшибками.количество === 0
     return errorFields.length === 0;
 }
 
+// форма.отправка(событиеОтправки => {
 $(".form").submit(e => {
-            e.preventDefault();
+    // событиеОтправки.запретитьСтандартноеПоведение()
+    e.preventDefault();
+    // форма = $(событиеОтправки.текущаяЦель)
+    const form = $(e.currentTarget);
+    // имя = форма.найти("[name='name']");
+    const name = form.find("[name='name']");
+    // телефон = форма.найти("[name='phone']");
+    const phone = form.find("[name='phone']");
+    const comment = form.find("[name='comment']");
+    const to = form.find("[name='to']");
+    // модальноеОкно = $("#modal");
+    const modal = $("#modal");
+    // контент = модальноеОкно.найти(".modal__content");
+    const content = modal.find(".modal__content");
+    // модальноеОкно.убратьКласс("modal-error");
+    modal.removeClass("modal-error");
 
-            const form = $(e.currentTarget);
-            const name = form.find("[name='name']");
-            const phone = form.find("[name='phone']");
-            const street = form.find("[name='street']");
-            const comment = form.find("[name='comment']");
-            const to = form.find("[name='to']");
+    // являетсяВалидной = валидироватьПоля(форма, [имя, телефон, комментарий, куда]);
+    const isValid = validateFields(form, [name, phone, comment, to]);
 
-            const modal = $("modal");
-            const content = modal.find(".modal__content");
+    // если форма является валидной
+    if (isValid) {
+        // const запрос = $.ajax({ // отправляет запрос по адресу
+        const request = $.ajax({
+            // адрес: "https://webdev-api.loftschool.com/sendmail",
+            url: "https://webdev-api.loftschool.com/sendmail",
+            // метод отправки данных
+            method: "post",
+            // отправляемые данные
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val(),
+            },
 
-            modal.removeClass("modal-error");
+        });
+        // request в данном случае это ответ от адреса, на который мы отправили данные
+        // запрос.завершен((данные) => {
+        request.done((data) => {
+            // контент.текст(данные.сообщение)
+            content.text(data.message);
+        });
+        // запрос.неудача((данные) => {
+        request.fail((data) => {
+            // сообщение = данные.ответJSON.сообщение
+            const message = data.responseJSON.message;
+            // контент.текст(сообщение)
+            content.text(message);
+            // модалка.добавитьКласс("modal-error");
+            modal.addClass("modal-error");
+        });
+        // при любом ответе
+        // запрос.всегда((данные) => {
+        request.always((data) => {
+            // фенсибокс.открыть
+            $.fancybox.open({
+                // источник: '#modal',
+                src: '#modal',
+                // тип : 'inline'
+                type: 'inline'
+            });
+        });
 
+    }
+    // {} -это объект
 
-            const isValid = validateFields(form, [name, phone, street, comment, to]);
-
-            // !!!![name, phone, street, comment, to].forEach(field => {
-            //     field.removeClass("input-error");
-            //     if (field.val().trim() === "") {
-            //         field.addClass("input-error");
-            //     }
-            // });
-
-            // const errorFields = form.find(".input-error");
-
-            if (isValid) {
-                const request = $.ajax({
-                    url: "https://webdev-api.loftschool.com/sendmail",
-                    method: "post",
-                    data: {
-                        name: name.val(),
-                        phomne: phone.val(),
-                        comment: comment.val(),
-                        to: to.val(),
-                    },
-                    success: data => {
-                        content.text(data.message)
-                            // console.log(data);
-                    },
-                    error: data => {
-                        const message = data.responseJSON.message;
-                        content.text(message);
-                        console.log(data);
-                        modal.addClass("modal-error");
-                        $.fancybox.open({
-                            src: '#modal',
-                            type: 'inline'
-                        });
-                    }
-                });
-
-
-            }
-            // {} -это объект
-
-            request.done((data) => {
-
-                }
-
-            );
-
-        $(".app-submit-button").click(e => {
-            e.preventDefault();
-
-            $.fancybox.close();
-        })
+});
+// кнопка закрытия модалки при клике
+$(".app-submit-button").click(e => {
+    e.preventDefault();
+    // фенсибокс.закрыть
+    $.fancybox.close();
+})
